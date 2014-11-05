e6e80fda44193f09b5851158b29229f99b5c0069
Ben <Ben@172-11-32-114.lightspeed.mmphtn.sbcglobal.net>
2014-11-05 14:29:18 +0800

    modify

---
+++ src/com/ai/demo/HelloWorld.java
@@ -0,0 +1,24 @@
+/**   
+* Copyright: Copyright (c) 2014 Asiainfo-Linkage
+* 
+* @ClassName: HelloWorld.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: Yang Bin
+* @date: 2014年11月5日 下午2:27:26 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2014年11月5日     Yang Bin           v1.0.0               修改原因
+*/
+package com.ai.demo;
+
+/**
+ * @author Yang Bin
+ *
+ */
+public class HelloWorld {
+
+}
